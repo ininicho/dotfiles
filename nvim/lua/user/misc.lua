f2dfd20075f2b5d7caef8378af4117fb86537c1c
Nicholaus Suprapto <nsuprapt@uwaterloo.ca>
2023-08-12 12:52:14 -0400

    lazyvim fresh install

--- nvim/lua/user/misc.lua
+++
@@ -1,8 +0,0 @@
--- Automatically regenerate compiled loader file on save
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile>
-  augroup end
-]])
-
